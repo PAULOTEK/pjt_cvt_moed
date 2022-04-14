1eb9e8d51dc7c5b059dedb9822798124a6b1ff41
PAULOTEK <pauloalexandre820@gmail.com>
2022-04-13 21:57:35 -0300

    Commit com ajustes

--- app/src/main/java/com/example/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.currencyconverter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private  RecyclerView.ViewHolder mViewHolder = RecyclerView.ViewHolder()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.mViewHolder.editValue = findViewById (R.id.editValor)
+        this.mViewHolder.textDolar = findViewById (R.id.textDolar)
+        this.mViewHolder.textEuro = findViewById (R.id.testeuro)
+        this.mViewHolder.ButtonCalcular = findViewById (R.id.btncalculate)
+        this.mViewHolder.ButtonCalcular.setOnClickListener(this)
+    }
+
+    override void onClick(View v) {}
+
+    private class ViewHolder{
+        EditText editValue
+        TextView textDolar
+        TextView textEuro
+        Button ButtonCalcular
     }
 }
